edefc0d8c13216406300990ed63a0a3eab10c0da
Kosmas Palios <kosmas.palios@gmail.com>
2022-07-12 17:49:04 +0300

    starting to add account registartion to call.py

--- PJSUA_py3_scripts/call.py
+++ PJSUA_py3_scripts/call.py
@@ -24,13 +24,13 @@ import pjsua as pj
 LOG_LEVEL=3
 current_call = None
 
-# Logging callback
-def log_cb(level, str, len):
-    print(str, end=' ')
+lib = pj.Lib()
 
 
 # Callback to receive events from account
 class MyAccountCallback(pj.AccountCallback):
+
+    sem = None
 
     def __init__(self, account=None):
         pj.AccountCallback.__init__(self, account)
@@ -51,6 +51,17 @@ class MyAccountCallback(pj.AccountCallback):
         current_call.set_callback(call_cb)
 
         current_call.answer(180)
+
+
+    def wait(self):
+        self.sem = threading.Semaphore(0)
+        self.sem.acquire()
+
+    def on_reg_state(self):
+        if self.sem:
+            if self.account.info().reg_status >= 200:
+                self.sem.release()
+
 
         
 # Callback to receive events from Call
@@ -166,5 +177,38 @@ except pj.Error as e:
     print("Exception: " + str(e))
     lib.destroy()
     lib = None
+
+
+import sys
+import pjsua as pj
+import threading
+
+
+def log_cb(level, str, len):
+    print(str, end=' ')
+
+try:
+    lib.init(log_cfg = pj.LogConfig(level=4, callback=log_cb))
+    lib.create_transport(pj.TransportType.UDP, pj.TransportConfig(5080))
+    lib.start()
+
+    acc = lib.create_account(pj.AccountConfig("compute.ilsp.gr", "kosmas", "opensips"))
+
+    acc_cb = MyAccountCallback(acc)
+    acc.set_callback(acc_cb)
+    acc_cb.wait()
+
+    print("\n")
+    print("Registration complete, status=", acc.info().reg_status, \
+          "(" + acc.info().reg_reason + ")")
+    print("\nPress ENTER to quit")
+    sys.stdin.readline()
+
+    lib.destroy()
+    lib = None
+
+except pj.Error as e:
+    print("Exception: " + str(e))
+    lib.destroy()
 
 
