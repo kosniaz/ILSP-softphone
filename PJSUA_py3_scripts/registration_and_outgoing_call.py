bfead88c71d959e0693c70e0737dd12d492dd6d5
kosmas <kosmas.palios@gmail.com>
2022-07-13 18:51:34 +0300

    Added new script for registration and call

---
+++ PJSUA_py3_scripts/registration_and_outgoing_call.py
@@ -0,0 +1,102 @@
+# $Id: registration.py 2171 2008-07-24 09:01:33Z bennylp $
+#
+# SIP account and registration sample. In this sample, the program
+# will block to wait until registration is complete
+#
+# Copyright (C) 2003-2008 Benny Prijono <benny@prijono.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+#
+import sys
+import pjsua as pj
+import threading
+import time
+
+def log_cb(level, str, len):
+    
+    print(str, end=' ')
+
+class MyAccountCallback(pj.AccountCallback):
+    sem = None
+
+    def __init__(self, account):
+        pj.AccountCallback.__init__(self, account)
+
+    def wait(self):
+        self.sem = threading.Semaphore(0)
+        self.sem.acquire()
+
+    def on_reg_state(self):
+        if self.sem:
+            if self.account.info().reg_status >= 200:
+                self.sem.release()
+
+class MyCallCallback(pj.CallCallback):
+
+    def __init__(self, call=None):
+        pj.CallCallback.__init__(self, call)
+
+    # Notification when call state has changed
+    def on_state(self):
+        global current_call
+        print("Call with", self.call.info().remote_uri, end=' ')
+        print("is", self.call.info().state_text, end=' ')
+        print("last code =", self.call.info().last_code, end=' ') 
+        print("(" + self.call.info().last_reason + ")")
+        
+        if self.call.info().state == pj.CallState.DISCONNECTED:
+            current_call = None
+            print('Current call is', current_call)
+
+    # Notification when call's media state has changed.
+    def on_media_state(self):
+        if self.call.info().media_state == pj.MediaState.ACTIVE:
+            # Connect the call to sound device
+            call_slot = self.call.info().conf_slot
+            pj.Lib.instance().conf_connect(call_slot, 0)
+            pj.Lib.instance().conf_connect(0, call_slot)
+            print("Media is now active")
+        else:
+            print("Media is inactive")
+
+lib = pj.Lib()
+
+try:
+    lib.init(log_cfg = pj.LogConfig(level=4, callback=log_cb))
+    lib.create_transport(pj.TransportType.UDP, pj.TransportConfig(5080))
+    lib.start()
+
+    acc = lib.create_account(pj.AccountConfig(domain="compute.ilsp.gr:5062", username="kosmas", password="opensips" ))
+
+    acc_cb = MyAccountCallback(acc)
+    acc.set_callback(acc_cb)
+    acc_cb.wait()
+
+    print("\n")
+    print("Registration complete, status=", acc.info().reg_status, \
+          "(" + acc.info().reg_reason + ")")
+    print("\nPress ENTER to make a call")
+    sys.stdin.readline()
+    if sys.argv[1]:
+        current_call=acc.make_call(sys.argv[1], cb=MyCallCallback())
+    time.sleep(10)
+    #lib.destroy()
+    #lib = None
+
+except pj.Error as e:
+    print("Exception: " + str(e))
+    lib.destroy()
+
+
